69849a999c98e7b69070359d3d2974b17cc040aa
Admin <758378737@qq.com>
2020-12-18 19:21:06 +0800

    2020-12-18 19:20:38

---
+++ app/src/main/java/com/feiniu/lifeai/ui/HomeActivity.java
@@ -0,0 +1,28 @@
+package com.feiniu.lifeai.ui;
+
+import com.feiniu.lifeai.R;
+import com.feiniu.lifeai.comm.MyActivity;
+
+/**
+ * Created by Admin
+ * Date 2020/12/16
+ */
+public class HomeActivity extends MyActivity {
+
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_home_layout;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
---
+++ app/src/main/java/com/feiniu/lifeai/ui/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.feiniu.lifeai.ui;
+
+import com.feiniu.lifeai.R;
+import com.feiniu.lifeai.comm.MyActivity;
+import com.gyf.immersionbar.BarHide;
+import com.gyf.immersionbar.ImmersionBar;
+import androidx.annotation.NonNull;
+
+/**
+ * 引导页
+ * Created by Admin
+ * Date 2020/12/16
+ */
+public class SplashActivity extends MyActivity {
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_splash_layout;
+    }
+
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+        startActivity(HomeActivity.class);
+    }
+
+    @NonNull
+    @Override
+    protected ImmersionBar createStatusBarConfig() {
+        return super.createStatusBarConfig().hideBar(BarHide.FLAG_HIDE_BAR);
+    }
+}
